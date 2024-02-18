da774b3cb22914ad66284129b58dc6be4da7acd7
KaynSuo <nadirxl@hotmail.fr>
2024-02-18 16:04:27 +0100

    creation projet Android

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+t
+Dapp/src/main/java/fr/upjv/asi_client_agenda_sportive/MainActivity.kt,d/7/d70de827a9144f4f151be3f001de7fd088da141d
---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ASI-Client-Agenda-Sportive"
+include(":app")
